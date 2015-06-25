2de24e9cc4ef98d5399c6ddc84e82386e6ab2b45
davecaos <david_bluetrain@hotmail.com>
2015-06-25 17:45:26 -0300

    [#5] indentation changes in src/trails.erl

--- src/trails.erl
+++ src/trails.erl
@@ -1,13 +1,14 @@
 -module(trails).
 
--export([compile/1]).
--export([single_host_compile/1]).
 
--spec compile(cowboy_router:routes()) -> cowboy_router:dispatch_rules().
-compile(Routes) ->
-  cowboy_router:compile(Routes).
+-export([single_host_compile/1]).
+-export([compile/1]).
 
 -spec single_host_compile([cowboy_router:route_path()]) ->
   cowboy_router:dispatch_rules().
 single_host_compile(Trails) ->
   compile([{'_', Trails}]).
+
+-spec compile(cowboy_router:routes()) -> cowboy_router:dispatch_rules().
+compile(Routes) ->
+  cowboy_router:compile(Routes).

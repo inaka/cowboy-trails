b3dab953b871ab3b3e79a0799eefb70c7df2c9dc
davecaos <david_bluetrain@hotmail.com>
2015-06-25 14:57:14 -0300

    [#4] Add trails basic tests to Common Test Suite

--- test/trails_SUITE.erl
+++ test/trails_SUITE.erl
@@ -4,6 +4,9 @@
 -export([all/0]).
 -export([init_per_suite/1]).
 -export([end_per_suite/1]).
+-export([basic_compile_test/1]).
+-export([minimal_compile_test/1]).
+-export([static_compile_test/1]).
 
 -type config() :: [{atom(), term()}].
 
@@ -27,3 +30,53 @@ end_per_suite(Config) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Test Cases
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec minimal_compile_test(config()) -> config().
+minimal_compile_test(Config) ->
+  MininalRoute = [{'_',[]}],
+  ExpectedResponse = [{'_',[],[]}],
+  ExpectedResponse = trails:compile(MininalRoute),
+  Config.
+
+-spec basic_compile_test(config()) -> config().
+basic_compile_test(Config) ->
+  BasicRoute =
+    [
+      {"localhost",
+        [
+          {"/such/path", http_such_path_handler, []},
+          {"/very", http_very, []},
+          {"/", http_handler, []}
+       ]}
+    ],
+  ExpectedResponse =
+    [{
+      [<<"localhost">>], [],
+      [
+        {[<<"such">>,<<"path">>], [], http_such_path_handler,[]},
+        {[<<"very">>], [], http_very, []},
+        {[], [], http_handler, []}
+      ]}
+    ],
+  ExpectedResponse = trails:compile(BasicRoute),
+  Config.
+
+-spec static_compile_test(config()) -> config().
+static_compile_test(Config) ->
+  StaticRoute =
+    [
+      {'_',
+        [
+          {"/", cowboy_static, {private_file, something, "index.html"}}
+        ]}
+    ],
+  ExpectedResponse =
+    [
+      {'_', [],
+        [
+          {[], [],cowboy_static, {private_file, something, "index.html"}}
+        ]}
+    ],
+  ExpectedResponse = trails:compile(StaticRoute),
+  Config.
+

f114d1ccdbf43f2d791ca05acc400bc6f23ee2d0
Turtle <deng@emqx.io>
2021-07-26 15:24:49 +0800

    add trails/1 callback

--- src/trails_handler.erl
+++ src/trails_handler.erl
@@ -8,6 +8,7 @@
 
 %% @doc Returns the cowboy routes defined in the called module.
 -callback trails() -> trails:trails().
+-callback trails(Opts :: map()) -> trails:trails().
 
 -spec trails(module() | {module(), map()}) -> trails:trails().
 trails({Module, Opts}) -> Module:trails(Opts);

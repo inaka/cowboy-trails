02b14e5428b1939d851988fcc8b15438538e3f19
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-01-06 23:24:23 +0000

    rebar3 format it

--- example/src/example_default.erl
+++ example/src/example_default.erl
@@ -1,34 +1,30 @@
 -module(example_default).
 
--export(
-   [
-    init/2,
-    content_types_accepted/2,
-    content_types_provided/2,
-    forbidden/2,
-    resource_exists/2
-   ]
-  ).
+-export([init/2, content_types_accepted/2, content_types_provided/2, forbidden/2,
+         resource_exists/2]).
 
 %% cowboy
 init(Req, State) ->
-  {cowboy_rest, Req, State}.
+    {cowboy_rest, Req, State}.
 
 content_types_accepted(Req, State) ->
-  {[{<<"text/plain">>, handle_put}], Req, State}.
+    {[{<<"text/plain">>, handle_put}], Req, State}.
 
 content_types_provided(Req, State) ->
-  {[{<<"text/plain">>, handle_get}], Req, State}.
+    {[{<<"text/plain">>, handle_get}], Req, State}.
 
 forbidden(Req, State) ->
-  {false, Req, State}.
+    {false, Req, State}.
 
 resource_exists(Req, State) ->
-  case cowboy_req:binding(key, Req) of
-    undefined -> {true, Req, State};
-    Key ->
-      case application:get_env(example, Key, undefined) of
-        undefined -> {false, Req, State};
-        _ -> {true, Req, State}
-      end
-  end.
+    case cowboy_req:binding(key, Req) of
+        undefined ->
+            {true, Req, State};
+        Key ->
+            case application:get_env(example, Key, undefined) of
+                undefined ->
+                    {false, Req, State};
+                _ ->
+                    {true, Req, State}
+            end
+    end.
--- example/src/example_description_handler.erl
+++ example/src/example_description_handler.erl
@@ -1,27 +1,17 @@
 -module(example_description_handler).
 
 -include_lib("mixer/include/mixer.hrl").
--mixin([
-        {example_default,
-         [
-          init/2,
-          content_types_accepted/2,
-          content_types_provided/2,
-          resource_exists/2
-         ]}
-       ]).
 
--export(
-  [
-   allowed_methods/2,
-   handle_get/2
-  ]).
+-mixin([{example_default,
+         [init/2, content_types_accepted/2, content_types_provided/2, resource_exists/2]}]).
+
+-export([allowed_methods/2, handle_get/2]).
 
 %% cowboy
 allowed_methods(Req, State) ->
-  {[<<"GET">>], Req, State}.
+    {[<<"GET">>], Req, State}.
 
 %% internal
 handle_get(Req, State) ->
-  Body = trails:all(),
-  {io_lib:format("~p~n", [Body]), Req, State}.
+    Body = trails:all(),
+    {io_lib:format("~p~n", [Body]), Req, State}.
